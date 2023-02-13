94592c0f427b00659303692d016e3ed0b6607e87
Saadh S <atextbooksituation@gmail.com>
2023-02-13 17:03:42 -0500

    Trying code out. Starting week 4 coding

--- week_4/Coderbyte_1/stencil.py
+++ week_4/Coderbyte_1/stencil.py
@@ -4,19 +4,51 @@ from __future__ import annotations
 Reverse a string without the built in function to do so
 '''
 def reverse_str(s: str) -> str:
-  pass
+  # O(n), O()
+  stringy = []
+  for i in range(len(stringy)-1,-1,-1):
+    stringy.append(i)
+  return ''.join(stringy)
+
 
 '''
 Find the most frequent substring of length n in a string. If there’s a tie, return the alphabetically smallest.
 '''
 def most_freq_substring(s: str, n: int) -> str:
-  pass
+  # sliding window right?
+  # look at n length of char each loop
+  # 
+  # do ord for alphabetically smallest if there's a tie
+  sub_dict = {}
+  most_freq = 0
+  sub_set = {'a','b'}
+  sub_set.clear()
+  for i in range(len(s) - n):
+    subs = s[i:i+n]
+    sub_dict[subs] += 1
+    if sub_dict[subs] >= most_freq:
+      most_freq = sub_dict[subs]
+      sub_set.add(subs)
+  if len(sub_set) == 1:
+    return sub_set.pop()
+  else:
+    smallest = 0
+    for i in sub_set:
+      total = 0
+      for j in i:
+        total += ord(j)
 
 '''
 What is the alphabetically smallest subsequence of length n in a string? 
 For example, a string “agbf” and length 2 subsequence would be “ab”. Time complexity does not need to be optimal.
 '''
 def smallest_subsequence(s: str, n: int) -> str:
+  # outer loop across string:
+  # loop for each lowest char to n  
+  total = 0
+  for j in s:
+    for i in range(n):
+      total += ord(j)      
   pass
 
 '''
