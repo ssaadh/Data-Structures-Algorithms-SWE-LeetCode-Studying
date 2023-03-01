262cae807df13dd7c847e6a1dbaf2bcb29a7483a
Saadh S <atextbooksituation@gmail.com>
2023-03-01 17:33:12 -0500

    StringBuilder. A few coding Qs left

--- week_4/Coderbyte_2/string_builder.py
+++ week_4/Coderbyte_2/string_builder.py
@@ -6,12 +6,13 @@ class StringBuilder:
 
     def __init__(self, string = "", capacity = None):
         self.s = string
-        self.length = 0
+        for c in self.s:
+          self.copy.append(c)
         self.capacity = capacity
   
     # Returns the string that is built.
     def __str__(self) -> str:
-        pass
+        return ''.join(self.copy)
   
     # Appends s to the array in O(len(s)) at the end. 
     # Should raise an exception if over capacity.
@@ -19,13 +20,14 @@ class StringBuilder:
       length = len(s) + len(self.s)
       if self.capacity is not None and self.capacity < length:
         raise Exception
-      self.s.append(self.s)
-      self.length += length
+      for c in s:
+        self.copy.append(c)
+      return None
       
   
     # Returns the length of the string.
     def size(self) -> int:
-        return self.length
+      return len(self.copy)
     
     # Returns the character at location index. 
     def char_at(self, index: int) -> str:
@@ -33,16 +35,18 @@ class StringBuilder:
           return self.s[index]
 
     def _bound_checking(self, start, end):
-      return (start < end) and (0 <= start <= self.capacity - 1) and (1 <= end <= self.capacity)
+      return (start < end) and (0 <= start <= self.size() - 1) and (1 <= end <= self.size())
       
     # Deletes characters between start (inclusive) and end (exclusive). 
     # Should raise an exception if start, end are out of bounds.
     def delete(self, start: int, end = None) -> None:
       if end is None:
-        end = self.length - 1
+        end = self.length
 
       if self._bound_checking(start, end):
-        self.s = self.s[:start] + self.s[:end]
+        for c, index in range(start, end):
+          del self.copy[index]
+        # self.copy = self.copy[:start] + self.copy[:end]
         return None
       else:
         raise Exception
@@ -52,17 +56,29 @@ class StringBuilder:
     # Should raise an exception if start, end are out of bounds.
     def substring(self, start: int, end = None) -> str:
       if end is None:
-        end = self.length - 1
-        
+        end = self.length
+
+      tmp = []
       if self._bound_checking(start, end):
-        return self.s[start:end]
+        for c in range(start, end):
+          tmp.append(c)
+        return tmp
+        # return self.s[start:end]
       else:
         raise Exception
 
     # Reverses the current string
     def reverse(self) -> str:
-        pass
+      copy = []
+      for c in range(0,1,-1):
+        copy.append(self.copy[c])
+      return copy
 
     # Replaces all occurrences of “old” with “new” 
     def replace(self, old: str, new: str):
-        pass
+      s = ''.join(self.copy)
+      s.replace(old, new)
+      self.copy = []
+      for c in s:
+        self.copy.append(c)
+      return None
