1f6ce719924d271b672916fee035bbbaf671a564
Saadh S <atextbooksituation@gmail.com>
2023-03-04 21:23:51 -0500

    As far as I’ll get without testing

--- week_4/Coderbyte_1/stencil.py
+++ week_4/Coderbyte_1/stencil.py
@@ -14,45 +14,59 @@ def reverse_str(s: str) -> str:
 '''
 Find the most frequent substring of length n in a string. If there’s a tie, return the alphabetically smallest.
 '''
-# @TODO finish
 def most_freq_substring(s: str, n: int) -> str:
-  # sliding window right?
-  # look at n length of char each loop
-  # 
-  # do ord for alphabetically smallest if there's a tie
-  sub_dict = {}
-  most_freq = 0
-  # so sub_set is init as a set
-  sub_set = {'a','b'}
-  sub_set.clear()
-  for i in range(len(s) - n):
-    subs = s[i:i+n]
-    sub_dict[subs] += 1
-    if sub_dict[subs] >= most_freq:
-      most_freq = sub_dict[subs]
-      sub_set.add(subs)
-  if len(sub_set) == 1:
-    return sub_set.pop()
-  else:
-    smallest = 0
-    for i in sub_set:
-      total = 0
-      for j in i:
-        total += ord(j)
+  sub_dict = dict()
+  for i in range(len(s)-n+1):
+    substring = s[i:i+n]
+    if substring in sub_dict:
+      sub_dict[substring] += 1
+    else:
+      sub_dict[substring] = 1
+  max_count = max(sub_dict.values())
+  final_list = []
+  for sub, count in sub_dict.items():
+    if count == max_count:
+      final_list.append(sub)
+  return min(final_list)
+
 
 '''
 What its the alphabetically smallest subsequence of length n in a string? 
 For example, a string “agbf” and length 2 subsequence would be “ab”. Time complexity does not need to be optimal.
 '''
+# @TODO finish -- Have to keep into account the length of the array to know whether to go for the smallest letter or for a letter to get the correct number of index in time. No clue!
 def smallest_subsequence(s: str, n: int) -> str:
-  # outer loop across string:
-  # loop for each lowest char to n  
-  total = 0
-  for j in s:
-    if len(total) == n:
-      return total
-    total += ord(j)
-  pass
+  if n == 1:
+    return min(s)
+  elif n >= len(s):
+    return s
+
+  sub = ''
+  # number of subsequences
+  for i in range(n):
+    least = float('inf')
+    stringy = ''
+    indexy = 0
+    # loop across string:
+    for index, j in enumerate(s):
+      if ord(j) < least:
+        least = ord(j)
+        stringy = j
+        indexy = index
+    sub += stringy
+    s = s[indexy + 1:]
+  return sub
+
+def smallest_subsequence_r(s, n):
+  if n == 1:
+    return min(s)
+  elif n >= len(s):
+    return s
+  else:
+    left = smallest_subsequence_r(s[1:], n)
+    right = s[0] + smallest_subsequence_r(s[1:], n - 1)
+    return min(a, b)
+
 
 '''
 Merge N sorted lists.
@@ -64,7 +78,6 @@ Example
 Input: [[1, 5, 8], [0, 2, 10], [4, 8, 9]]
 Output: [0, 1, 2, 4, 5, 8, 8, 9, 10]
 '''
-
 # Other idea: loop around all the arrays at each index. Sort these in a new array?
 def merge_n_lists(lst: list[list[int]]) -> list[int]:
   k = len(lsts)
@@ -100,6 +113,7 @@ def merge_n_lists(lst: list[list[int]]) -> list[int]:
     new_arr.append(right[p2])
     p2 += 1
   return new_arr
+
 
 ''' 
 Given a list of coordinates, sort them by increasing order for X values, then decreasing order for Y values
