e212842d68d025629bad00c9290793225e63f4e9
Saadh S <atextbooksituation@gmail.com>
2023-10-11 15:49:04 -0400

    Did heap Coderbyte 2 but without explanations or full understanding yet
    I’m still confused about max and min and the default heapq. I also need to verify I understand the underlying list when doing heaps and their functions.
    
    I believe I understand it because of the Coachable lectures and reading, but yeah.
    
    So like remembering ascending and descending. The default heapq heapify and heappush are always putting the smallest first/at the top/where [0] or lower index is. This is also what gets popped first, right? So by default it is ascending?

--- week_6/Coderbyte_2/stencil_1_workflow.py
+++ week_6/Coderbyte_2/stencil_1_workflow.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 import heapq
 
+# import pytest
+# do not modify this function call
+# retcode = pytest.main(['-v'])
+
 '''
 Complete the StreamHandlerKLargest class that has a capacity k by filling in the methods.
 '''
@@ -8,47 +12,76 @@ Complete the StreamHandlerKLargest class that has a capacity k by filling in the
 class StreamHandlerKLargest:
   def __init__(self, k: int) -> None:
     self.k = k
+    self.heap = []
+
+  # TODO go thru and trace the heappush and heappushpop
+  # TODO this will give deeper understanding you dont have right now!
+  # TODO understand ascending and decending and the defaults!
   
   '''
   This method adds the stream element to the collection. 
   You only need to store the k largest elements seen so far at any given point in time.
   '''
-  def add_stream_element(self, e: int) -> None: 
-    pass
+  def add_stream_element(self, e: int) -> None:
+    if len(self.heap) < self.k:
+      heapq.heappush(self.heap, e)
+    elif len(self.heap) == self.k:
+      heapq.heappushpop(self.heap, e)
+    return None
 
   ''' 
   This method returns the k largest elements seen so far.
   '''
-  def k_largest(self) -> list[int]: 
-    return []
-
-'''
-Complete the StreamHandlerKSmallest class that has a capacity k by filling in the methods.
-'''
+  def k_largest(self) -> list[int]:
+    if len(self.heap) < self.k:
+      return self.heap
+    return self.heap[:self.k]
+  
+# TODO explain the differences
 
 class StreamHandlerKSmallest:
   def __init__(self, k: int) -> None:
     self.k = k
+    self.heap = []
   
   '''
   This method adds the stream element to the collection. 
   You only need to store the k smallest elements seen so far at any given point in time.
   '''
   def add_stream_element(self, e: int) -> None: 
-    pass
+    if len(self.heap) < self.k:
+      heapq.heappush(self.heap, -e)
+    elif len(self.heap) == self.k:
+      heapq.heappushpop(self.heap, -e)
+    return None
 
   ''' 
   This method returns the k smallest elements seen so far.
   '''
   def k_smallest(self) -> list[int]: 
-    return []
-
+    if len(self.heap) < self.k:
+      return [-m for m in self.heap]
+    return [-m for m in self.heap[:self.k]]
+  
 ''' 
 Write a function that creates a copy of the list, and sorts it in ascending order
 using a heap.
 '''
+
+# TODO Trace thru
+# Talk to Coachable-AI
+# Go through the input list and add each to a heap
+# Or heapify can do the whole list in one go?
+# Heapify will order it from smallest to largest which is ascending (ascend up)
+
 def heapsort(input_list: list[int]) -> list[int]: 
-  return []
+  final = []
+  heapq.heapify(input_list)
+  while input_list:
+    next_up = heapq.heappop(input_list)
+    final.append(next_up)
+  return final
+
 
 '''
 Suppose we have some data that can be expressed as a tuple (a, b, c). 
@@ -57,6 +90,9 @@ where k <= n. Each datapoint has a score, defined as 2*a + 5*b + 10*c,
 and the higher the score, the greater the datapoint. 
 Complete the class for this datapoint object, and complete the below function, using a heap to do so.
 '''
+
+# The heap has to have the score value stored for the heap to automatically compare them
+
 class Datapoint:
   def __init__(self, a: int, b: int, c: int) -> None:
     self.a = a
@@ -78,6 +114,8 @@ class Datapoint:
 # Return them as tuples, using the to_tuple method in the Datapoint class.
 def get_top_k_datapoints(data_collection: list[Datapoint], k: int) -> set[tuple[int, int, int]]:
   heapq.heapify(data_collection)
+  for m in data_collection:
+    print(m.to_tuple(), m.score())
   final = set()
   for m in range(k):
     final.add(heapq.heappop(data_collection).to_tuple())
